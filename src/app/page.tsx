02cdcf1c12f165370ec6e3d4e8d5efdf59f7b73f
vanhungtran <tranvanhungvx@gmail.com>
2025-11-11 20:49:10 +0700

    update products page and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,6 @@ export default function HomePage() {
       <SlideSection />
       <FreeGift />
       <DoubleImageSection />
-      <AllProductsSection />
-      <ProductsGridSection />
     </>
   );
 }
